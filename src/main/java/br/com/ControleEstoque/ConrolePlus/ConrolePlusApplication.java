8568982a3e7eedac5f0b67a438dbdc728c61f6e0
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-02-20 10:33:11 -0300

    Subindo projeto

---
+++ src/main/java/br/com/ControleEstoque/ConrolePlus/ConrolePlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.ControleEstoque.ConrolePlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConrolePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConrolePlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ControleEstoque/ConrolePlus/ConrolePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ControleEstoque.ConrolePlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConrolePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

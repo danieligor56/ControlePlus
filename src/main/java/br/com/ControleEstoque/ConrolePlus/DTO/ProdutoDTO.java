e0f237c780b0fbd4a7843af2778cf1014c03705a
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-02-20 18:04:08 -0300

    Criando DTO

---
+++ src/main/java/br/com/ControleEstoque/ConrolePlus/DTO/ProdutoDTO.java
@@ -0,0 +1,15 @@
+package br.com.ControleEstoque.ConrolePlus.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProdutoDTO {
+	
+		private String nome;
+		
+}

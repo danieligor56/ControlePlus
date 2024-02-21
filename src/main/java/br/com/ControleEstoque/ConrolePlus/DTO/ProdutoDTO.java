89f93f027480ac1f5df431221ef16a31c8ac5481
danieligor56 <52861750+danieligor56@users.noreply.github.com>
2024-02-21 20:15:59 -0300

    Criação de classe DTO - OK

--- src/main/java/br/com/ControleEstoque/ConrolePlus/DTO/ProdutoDTO.java
+++ src/main/java/br/com/ControleEstoque/ConrolePlus/DTO/ProdutoDTO.java
@@ -3,7 +3,9 @@ package br.com.ControleEstoque.ConrolePlus.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,5 +13,9 @@ import lombok.NoArgsConstructor;
 public class ProdutoDTO {
 	
 		private String nome;
+		private String descricao;
+		private Long codBarra;
+		private Double prcIte;
+		private int qtdIte;
 		
-}
+				}
